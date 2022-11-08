b9a9874e194c6df945589abdcf5e3ced1b25b1b1
Safikul Islam <islam.safikul@hotmail.com>
2022-11-08 08:09:13 +0530

    JWT token configuration added to Program.cs

--- Models/ApplicationUserModel.cs
+++ Models/ApplicationUserModel.cs
@@ -10,7 +10,9 @@ namespace PixyDeltaApi.Models;
 public class ApplicationUser : IdentityUser
 {
     [Required]
+    [MaxLength(255)]
     public string? FirstName { get; set; }
     [Required]
+    [MaxLength(255)]
     public string? LastName { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PixyDeltaApi.Hubs;
 using PixyDeltaApi.Models;
@@ -5,14 +6,19 @@ using PixyDeltaApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCors(c => c.AddPolicy("default", builder =>
-{
-    builder
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader();
-}));
+builder.Services.AddCors(c => c.AddPolicy("default", builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }));
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequiredUniqueChars = 1;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.User.RequireUniqueEmail = true;
+
+}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,7 +35,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("default");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
